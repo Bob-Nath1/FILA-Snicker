d2746f13587ca02230e4cf84a75cb78ddf4149fa
Bob-Nath1 <124548828+Bob-Nath1@users.noreply.github.com>
2025-02-14 14:26:20 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# FILA-Snicker
-At Fila, we believe that every step you take is part of a bigger journey, whether you're conquering the streets, hitting the gym, or exploring the outdoors. Our shoes are designed to provide the perfect blend of style, comfort, and durability, ensuring that you move with confidence wherever life takes you.
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,39 @@
+const HeroSection = () => {
+
+    return (
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>YOUR JOURNEY, OUR SHOES.</h1>
+                <p>At Fila, we believe that every step you take is part of a bigger journey, whether you're conquering the streets, hitting the gym, or exploring the outdoors. Our shoes are designed to provide the perfect blend of style, comfort, and durability, ensuring that you move with confidence wherever life takes you.</p>
+
+                <div className="hero-btn">
+            <button>Shop Now</button>
+            <button className="secondary-btn">Category</button>
+        </div>
+
+        <div className="shopping">
+            <p>Also Available On</p>
+
+            <div className="brand-icons">
+                <img src="./image/Amazon logo.png" alt="amazon logo" />
+                <img src="./image/Flipkart 2.jpg" alt="Flipkart logo" />
+            </div>
+            </div>
+            </div>
+
+            <div className="hero-image">
+                <img src="./image/Fila foot wear.jpg" alt="Fila picture" />
+            </div>
+
+            
+            
+            
+        </main>  
+        
+
+        
+    )
+};
+
+export default HeroSection;
+
